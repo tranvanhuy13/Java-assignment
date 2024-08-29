503c76039f5abf9f0cb87b535fe5d88a093d32ec
tranvanhuy13 <161944282+tranvanhuy13@users.noreply.github.com>
2024-08-29 11:02:32 +0700

    Delete src directory

--- src/array_loop.java
+++
@@ -1,8 +0,0 @@
-import java.util.Arrays;
-public class array_loop {
-    public static void main(String[] args) {
-        int[] e = {0,2,4,6,8};
-        e[1]=e[3]+e[4];
-        System.out.println("e is "+ Arrays.toString(e));
-    }
-}
